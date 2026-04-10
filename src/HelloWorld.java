aad687bc5459de1a315432aa6cf1cc8400a67bd0
madhusamala-dev <madhu@trainingmug.com>
2026-04-10 19:52:35 +0530

    initial-commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hey, Welcome to Java!");
+    }
+}
+class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Greeting from StSchool !");
+    }
+}
